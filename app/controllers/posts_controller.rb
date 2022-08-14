b8f11a255333f609217dff0526c371f767b2d175
gozooit <gozoo.it@protonmail.com>
2022-08-14 14:42:56 +0200

    solved

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,28 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: %i[new create]
 
   def new
-    @posts = Post.new
+    @post = Post.new
   end
 
   def create
-    @post = Post.new(user_id: current_user)
+    @post = Post.new(post_params)
+    @post.user = current_user
     if @post.save
+      flash[:notice] = 'Post added!'
       redirect_to new_post_path
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
